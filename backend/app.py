393331b9df704f5b1d88dd0d0bf354ca19befc50
LIZ <lilybouka45@gmail.com>
2026-06-15 21:57:51 +0200

    Prepare backend deployment

--- backend/app.py
+++ backend/app.py
@@ -1,42 +1,26 @@
-# Import du framework Flask pour créer l'application web
+
 from flask import Flask
 
-# Extension permettant d'autoriser les requêtes cross-origin
-# (nécessaire pour que le frontend React puisse communiquer avec l'API)
+# autoriser les requêtes cross-origin
 from flask_cors import CORS
-
-# Permet de charger les variables d’environnement (.env)
+#  variables d’environnement .env
 from dotenv import load_dotenv
-
-# Import de la configuration personnalisée
+#  configuration personnalisée
 from config import Config
-
-# Import de l'instance SQLAlchemy (gestion base de données)
+#  SQLAlchemy 
 from extensions import db
 
-# Import des blueprints (séparation des routes par modules)
+# Import des blueprints 
 from routes_products import bp_products
 from routes_reviews import bp_reviews
 
 
 def create_app():
-    """
-    Factory function qui crée et configure l'application Flask.
-    Cette structure est recommandée pour les projets modulaires.
-    """
-
-    # Chargement des variables d’environnement
     load_dotenv()
-
-    # Création de l’application Flask
     app = Flask(__name__)
-
-    # Chargement des paramètres depuis la classe Config
+    # Chargement  depuis la classe Config
     app.config.from_object(Config)
-
     # Configuration du CORS :
-    # Autorise les requêtes venant du frontend (React)
-    # uniquement sur les routes commençant par /api/*
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     # Initialisation de la base de données avec l'application
@@ -47,7 +31,7 @@ def create_app():
         db.create_all()
 
     # Enregistrement des blueprints
-    # Cela permet d’organiser les routes par fonctionnalité
+   
     app.register_blueprint(bp_products)
     app.register_blueprint(bp_reviews)
 
@@ -60,8 +44,8 @@ def create_app():
 
 
 # Point d’entrée de l’application
-if __name__ == "__main__":
-    app = create_app()
 
-    # Lancement du serveur en mode debug (développement)
+
+app = create_app()
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
